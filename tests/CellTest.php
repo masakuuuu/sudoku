d32adc16d4f12f9a984ccb50c9f1843ff497ce79
MasaKu <masakikuyama@gmail.com>
2023-06-25 14:25:36 +0900

    cellTest refactoring

--- tests/CellTest.php
+++ tests/CellTest.php
@@ -7,7 +7,11 @@ final class CellTest extends TestCase
 {
     public function testCell(): void
     {
-        $cell = new Cell(1, 3, 2);
+        $cell = new Cell();
+        $cell->number = 1;
+        $cell->row = 3;
+        $cell->column = 2;
+
         $this->assertSame(1, $cell->number);
         $this->assertSame(3, $cell->row);
         $this->assertSame(2, $cell->column);
